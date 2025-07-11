ab229010d36854fd4e2e323bbe3461607df0330f
giannag27 <giacosey28@gmail.com>
2025-07-11 12:33:02 -0400

    trying to print out emoji results. taking break.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -7,7 +7,7 @@ round = (0)
 while round < 3:
     player = int(input("Pick a number between 1 and 3. "))
     round = round + 1
-    
+
     print("✊")
     print("✋")
     print("✌️")
@@ -21,28 +21,28 @@ while round < 3:
     computer = random.randint(1, 3)
 
     if computer == 1 and player == 2:
-        print(player)
-        print(computer)
+        print(2)
+        print(1)
         print("You Win!")
     elif computer == 2 and player == 3:
-        print(player)
-        print(computer)
+        print(3)
+        print(2)
         print("You Win!")
     elif computer == 3 and player == 1:
-        print(player)
-        print(computer)
+        print(1)
+        print(3)
         print("You Win!")
     elif computer == 2 and player == 1:
-        print(player)
-        print(computer)
+        print(1)
+        print(2)
         print("Computer Wins!")
     elif computer == 3 and player == 2:
-        print(player)
-        print(computer)
+        print(2)
+        print(3)
         print("Computer Wins!")
     elif computer == 1 and player == 3:
-        print(player)
-        print(computer)
+        print(3)
+        print(1)
         print("Computer Wins!")
     else:
         print("It's a tie!")
