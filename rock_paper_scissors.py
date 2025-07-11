4bd173403176aa9dd2454d69a818faaa5252d59e
giannag27 <giacosey28@gmail.com>
2025-07-11 13:07:12 -0400

    Finished rock, paper, scissors!

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -4,45 +4,45 @@ player = (0)
 computer = (0)
 round = (0)
 
+1 == ("✊")
+2 == ("✋")
+3 == ("✌️")
+
 while round < 3:
     player = int(input("Pick a number between 1 and 3. "))
     round = round + 1
 
-    print("✊")
-    print("✋")
-    print("✌️")
+    print(" 1) ✊")
+    print(" 2) ✋")
+    print(" 3) ✌️")
     print("Shoot!")
-
-    1 == ("✊")
-    2 == ("✋")
-    3 == ("✌️")
 
     import random
     computer = random.randint(1, 3)
 
     if computer == 1 and player == 2:
-        print(2)
-        print(1)
+        print("Player: ✋")
+        print("CPU: ✊")
         print("You Win!")
     elif computer == 2 and player == 3:
-        print(3)
-        print(2)
+        print("Player: ✌️")
+        print("CPU: ✋")
         print("You Win!")
     elif computer == 3 and player == 1:
-        print(1)
-        print(3)
+        print("Player: ✊")
+        print("CPU: ✌️")
         print("You Win!")
     elif computer == 2 and player == 1:
-        print(1)
-        print(2)
+        print("Player: ✊")
+        print("CPU: ✋")
         print("Computer Wins!")
     elif computer == 3 and player == 2:
-        print(2)
-        print(3)
+        print("Player: ✋")
+        print("CPU: ✌️")
         print("Computer Wins!")
     elif computer == 1 and player == 3:
-        print(3)
-        print(1)
+        print("Player: ✌️")
+        print("CPU: ✊")
         print("Computer Wins!")
     else:
         print("It's a tie!")
